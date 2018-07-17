6d9243bd44fa2e28e3148f79ea19627c42f32993
Romain <romainsaillour@orange.fr>
2018-07-17 17:56:41 +0200

    ajout du Readme

---
+++ test_player.rb
@@ -0,0 +1,13 @@
+
+
+        p "Name of the player 1 : "
+        p "> "
+        player_name = gets.chomp
+
+
+        p "Player 1 is now #{player_name}"
+
+        p "Name of the player 2 : "
+        p "> "
+        player_name = gets.chomp
+        p "Player 2 is now #{player_name}"
