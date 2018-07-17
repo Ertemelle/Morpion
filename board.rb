62e70f2d570542b33084a1b64da78e33b03c8b04
Ertemelle <phenixime@hotmail.fr>
2018-07-17 18:26:44 +0200

    ajout de commentaires

--- board.rb
+++ board.rb
@@ -1,27 +1,32 @@
 class Board
-	attr_accessor :grid
-	def initialize
-		
-	@grid = *(1..9)
-	
-	end
+    attr_accessor :grid
 
-	def display
-		puts "#{@grid[0..2].join(" | ")}"
-		puts "--|---|--"
-		puts "#{@grid[3..5].join(" | ")}"
-		puts "--|---|--"
-		puts "#{@grid[6..8].join(" | ")}"
+    def initialize
 
-	end
-		def winning_condition?
-		grid[0] == grid[1] && grid[1] == grid[2] ||
-		grid[3] == grid[4] && grid[4] == grid[5] ||
-		grid[6] == grid[7] && grid[7] == grid[8] ||
-		grid[0] == grid[3] && grid[3] == grid[6] ||
-		grid[1] == grid[4] && grid[4] == grid[7] ||
-		grid[2] == grid[5] && grid[5] == grid[8] ||
-		grid[0] == grid[4] && grid[4] == grid[8] ||
-		grid[2] == grid[4] && grid[4] == grid[6]
-	end
+    #grille possedant 9 case avec les chiffres 1 a 9
+    @grid = *(1..9)
+
+    end
+
+    #faire apparaitre la grille avec les chiffres
+    def display
+        puts “#{@grid[0..2].join(” | “)}”
+        puts “--|---|--”
+        puts “#{@grid[3..5].join(” | “)}”
+        puts “--|---|--”
+        puts “#{@grid[6..8].join(” | “)}”
+
+    end
+
+        #methode comportant les differentes possibilites de victoires (8)
+        def winning_condition?
+        grid[0] == grid[1] && grid[1] == grid[2] ||
+        grid[3] == grid[4] && grid[4] == grid[5] ||
+        grid[6] == grid[7] && grid[7] == grid[8] ||
+        grid[0] == grid[3] && grid[3] == grid[6] ||
+        grid[1] == grid[4] && grid[4] == grid[7] ||
+        grid[2] == grid[5] && grid[5] == grid[8] ||
+        grid[0] == grid[4] && grid[4] == grid[8] ||
+        grid[2] == grid[4] && grid[4] == grid[6]
+    end
 end
--- game.rb
+++ game.rb
@@ -1,70 +1,84 @@
-require_relative 'board'
-require_relative 'player'
+require_relative ‘board’ #permet de relier le fichier board avec game.rb
+require_relative ‘player’ #permet de relier le fichier player avec game.rb
 
 class Game
 
-	def initialize
-		p "Welcome to my amazing game : "
-		p "========== TIC TAC TOE =========="
+    def initialize
+        p “Welcome to my amazing game : ”
+        p “========== TIC TAC TOE ==========”
 
 
-		p "Name of the player 1 : "
-		name_1 = gets.chomp
-		@player1 = Player.new(name_1, "X")
+        p “Name of the player 1 : ”
+        #saisi du nom par le joueur 1
+        name_1 = gets.chomp
+        #le player 2 a le name_1 et le X
+        @player1 = Player.new(name_1, “X”)
 
-		p "Name of the player 2 : "
-		name_2 = gets.chomp
-		@player2 = Player.new(name_2, "0")
-		
-		p "Player 1 is now #{name_1}, player 2 is now #{name_2}"
-		p "#{name_1} use X, #{name_2} use 0"
+        p “Name of the player 2 : ”
+        #saisi du nom par le joueur 2
+        name_2 = gets.chomp
+        #le player 2 a le name_2 et le Ø
+        @player2 = Player.new(name_2, “Ø”)
 
-		@current_player = @player1
-		@board = Board.new
+        p “Player 1 is now #{name_1}, player 2 is now #{name_2}”
+        p “#{name_1} use X, #{name_2} use Ø”
 
-	end
+        #le premier joueur a jouer est le player1
+        @current_player = @player1
+        @board = Board.new
 
-	def switch_players
-		if @current_player == @player1
-			@current_player = @player2
-		else
-			@current_player = @player1
-		end
-	end
+    end
 
-	def move
-		puts "#{@current_player.name}, where do you want to move ? (puts a number between 1 and 9) : "
-		target = gets.chomp
-		if target.to_i == @board.grid[target.to_i - 1]
-			@board.grid[target.to_i - 1] = @current_player.marker
-		else
-			puts "You can't, please try again :)"
-			switch_players
-		end
-	end
+    #methode permettant le changement de joueur a chaque tour
+    def switch_players
+        if @current_player == @player1
+            @current_player = @player2
+        else
+            @current_player = @player1
+        end
+    end
 
-	def start
-		loop do
-			@board.display
-			move
-			if game_over?
-				break
-			else
-				switch_players
-			end
-		end
-		@board.display
-		if @board.winning_condition?
-			puts "Game over ! #{@current_player.name} won !"
-		else
-			puts "Game over ! It's a draw !"
-		end
-	end
 
-	def game_over?
-		@board.winning_condition? ||
-		@board.grid.all? {|cell| cell.is_a? String}
-	end
+    def move
+        puts “#{@current_player.name}, where do you want to move ? (puts a number between 1 and 9) : ”
+        target = gets.chomp
+
+        #on passe la saisie du joueur en integer
+        if target.to_i == @board.grid[target.to_i - 1]
+            @board.grid[target.to_i - 1] = @current_player.marker
+        else
+            #message d’erreur si le chiffre entre a deja ete prit ou si ce n’est pas compris entre 1 et 9
+            puts “You can’t, please try again :)”
+            switch_players
+        end
+    end
+
+    #methode start permettant de lancer le jeux
+    def start
+        loop do
+            @board.display
+            move
+            if game_over?
+                break
+            else
+                switch_players
+            end
+        end
+        @board.display
+        if @board.winning_condition?
+            puts “Game over ! #{@current_player.name} won !”
+        else
+            puts “Game over ! It’s a draw !”
+        end
+    end
+
+    #si une condition de winning_condition est vrai ou qu’il n’y a plus de case de libre. on renvoi game over
+    def game_over?
+        @board.winning_condition? ||
+        @board.grid.all? {|cell| cell.is_a? String}
+    end
 end
 
+#permet de lancer le jeux
 game = Game.new
+game.start
